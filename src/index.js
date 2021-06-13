4f61dede467c3200f49b1bd9851aab35a69b8394
bogvoicescu <bog.voicescu@gmail.com>
2021-06-13 22:36:41 +0300

    Added character cards

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+// import Card from './Card';
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import {robots} from './robots';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Hello />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <CardList robots={robots}/>
+  ,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
